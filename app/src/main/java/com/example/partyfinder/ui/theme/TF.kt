6057cea1ef33667cd6834a9b624c858313bd0bc2
Samarth <sammehta063@gmail.com>
2023-12-13 12:01:09 +0530

    loginSignUp

---
+++ app/src/main/java/com/example/partyfinder/ui/theme/TF.kt
@@ -0,0 +1,178 @@
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.interaction.Interaction
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material3.*
+import android.view.View
+import androidx.compose.material.icons.filled.MailOutline
+import androidx.compose.runtime.*
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.Visibility
+import com.example.partyfinder.R
+import com.example.partyfinder.ui.theme.PartyFinderTheme
+
+class TF : ComponentActivity() {
+    @ExperimentalComposeUiApi
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            PartyFinderTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    LoginForm()
+                }
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalComposeUiApi::class)
+@Composable
+fun LoginForm() {
+    var email by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+    var confirmPassword by remember { mutableStateOf("") }
+    var isPasswordVisible by remember { mutableStateOf(false) }
+    var isConfirmPasswordVisible by remember { mutableStateOf(false) }
+
+    val keyboardController = LocalSoftwareKeyboardController.current
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.download),
+            contentDescription = null,
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(380.dp)
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        OutlinedTextField(
+            value = email,
+            onValueChange = { email = it },
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(56.dp),
+            label = { Text("Enter Email") },
+            keyboardOptions = KeyboardOptions.Default.copy(
+                keyboardType = KeyboardType.Email
+            ),
+            leadingIcon = {
+                Icon(
+                    imageVector = Icons.Default.MailOutline,
+                    contentDescription = null
+                )
+            }
+        )
+
+        Spacer(modifier = Modifier.height(8.dp))
+
+        OutlinedTextField(
+            value = password,
+            onValueChange = { password = it },
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(56.dp),
+            label = { Text("Enter Password") },
+            keyboardOptions = KeyboardOptions.Default.copy(
+                keyboardType = KeyboardType.Password
+            ),
+            visualTransformation = if (isPasswordVisible) {
+                VisualTransformation.None
+            } else {
+                PasswordVisualTransformation()
+            },
+            trailingIcon = {
+                PasswordToggle(
+                    isVisible = isPasswordVisible,
+                    onToggle = { isPasswordVisible = it }
+                )
+            }
+        )
+
+        Spacer(modifier = Modifier.height(8.dp))
+
+        OutlinedTextField(
+            value = confirmPassword,
+            onValueChange = { confirmPassword = it },
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(56.dp),
+            label = { Text("Confirm Password") },
+            keyboardOptions = KeyboardOptions.Default.copy(
+                keyboardType = KeyboardType.Password
+            ),
+            visualTransformation = if (isConfirmPasswordVisible) {
+                VisualTransformation.None
+            } else {
+                PasswordVisualTransformation()
+            },
+            trailingIcon = {
+                PasswordToggle(
+                    isVisible = isConfirmPasswordVisible,
+                    onToggle = { isConfirmPasswordVisible = it }
+                )
+            }
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Button(
+            onClick = {
+                // Handle button click
+            },
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(56.dp)
+        ) {
+            Text("Log In")
+        }
+    }
+}
+
+@Composable
+fun PasswordToggle(isVisible: Boolean, onToggle: (Boolean) -> Unit) {
+    val image = if (isVisible) Icons.Default.Visibility else Icons.Default.VisibilityOff
+
+    IconButton(onClick = { onToggle(!isVisible) }) {
+        Icon(imageVector = image, contentDescription = null)
+    }
+}
+
+@Composable
+fun detectTapGlobally(function: Any) {
+    // Implement your tap detection logic here
+    // You may use pointerInput or other methods based on your requirements
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewLoginForm() {
+    PartyFinderTheme {
+        LoginForm()
+    }
+}

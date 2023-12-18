3934062eaa2b3e6835b727cc49ac77c125235cf5
Kaizoku22 <pdeepankar46@gmail.com>
2023-12-18 16:17:54 +0530

    Created the EditProfileScreen UI note that the UI is Static yet

--- app/src/main/java/com/example/partyfinder/ProfileScreen.kt
+++ app/src/main/java/com/example/partyfinder/ProfileScreen.kt
@@ -7,7 +7,6 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
@@ -33,7 +32,6 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.core.app.ComponentActivity
 import com.example.partyfinder.ui.theme.PartyFinderTheme
 
 

4cf11d3f46f969cef099aa164e37cb750ea92b47
Kaizoku22 <pdeepankar46@gmail.com>
2023-12-20 14:54:47 +0530

    Commit 9  Changed the largeLabel typography and added dimenstion resources

--- app/src/main/java/com/example/partyfinder/ui/theme/Type.kt
+++ app/src/main/java/com/example/partyfinder/ui/theme/Type.kt
@@ -2,10 +2,10 @@ package com.example.partyfinder.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
-import androidx.compose.ui.text.font.Font
 import com.example.partyfinder.R
 
 
@@ -89,9 +89,8 @@ val Typography = Typography(
     ),
 
     labelLarge = TextStyle(
-        fontFamily = Inter,
-        fontWeight = FontWeight.Bold,
-        fontSize = 16.sp
+        fontFamily = JuraBold,
+        fontSize = 12.sp
     ),
     labelMedium = TextStyle(
         fontFamily = Inter,

e15f8f0abb67108a48bda98f0e8193a5f56f5c3e
Samarth <sammehta063@gmail.com>
2023-12-27 12:40:01 +0530

    Commit16
    FInal touches on Nav bar Icon

--- app/src/main/java/com/example/partyfinder/TF.kt
+++ app/src/main/java/com/example/partyfinder/TF.kt
@@ -57,7 +57,6 @@ public fun TF(modifier: Modifier = Modifier) {
 
 //        NavigationBar
         Row(modifier = modifier
-            .height(52.dp)
             .fillMaxWidth()
             .background(color = colorResource(id = R.color.DarkBG))
             .align(Alignment.BottomCenter)
@@ -67,103 +66,174 @@ public fun TF(modifier: Modifier = Modifier) {
 
             Spacer(modifier = modifier.weight(1f))
 
-            Row(modifier = modifier
-                .background(color = colorResource(id = R.color.black),
-                    shape = RoundedCornerShape(6.dp))
-                .border(
-                    width = 1.dp,
-                    color = colorResource(id = R.color.CallWidgetBorder),
-                    shape = RoundedCornerShape(6.dp)
-                )
+            Column(modifier = modifier
+                .padding(top = 4.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
             ) {
-                Image(
-                    painter = painterResource(id = R.drawable.bookmark_blue),
-                    contentDescription = "Share",
+                Row(modifier = modifier
+                    .background(color = colorResource(id = R.color.black),
+                        shape = RoundedCornerShape(6.dp))
+                    .border(
+                        width = 0.5.dp,
+                        color = colorResource(id = R.color.CallWidgetBorder),
+                        shape = RoundedCornerShape(6.dp)
+                    )
+                ) {
+                    Image(
+                        painter = painterResource(id = R.drawable.networkingone_blue),
+                        contentDescription = "Share",
+                        modifier = modifier
+                            .padding(14.dp, 8.dp)
+                            .size(24.dp)
+                    )
+                }
+                Text(
+                    text = "Community",
+                    style = MaterialTheme.typography.titleSmall,
+                    color = colorResource(id = R.color.SubliminalText),
+                    fontSize = 10.sp,
                     modifier = modifier
-                        .padding(12.dp, 4.dp)
-                        .size(24.dp)
+//                            .padding(start = 12.dp, top = 2.dp, end = 16.dp)
+                )
+            }
+
+
+            Spacer(modifier = modifier.weight(1f))
+
+            Column(modifier = modifier
+                .padding(top = 4.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Row(modifier = modifier
+                    .background(color = colorResource(id = R.color.black),
+                        shape = RoundedCornerShape(6.dp))
+                    .border(
+                        width = 0.5.dp,
+                        color = colorResource(id = R.color.CallWidgetBorder),
+                        shape = RoundedCornerShape(6.dp)
+                    )
+                ) {
+                    Image(
+                        painter = painterResource(id = R.drawable.bookmark_blue),
+                        contentDescription = "Share",
+                        modifier = modifier
+                            .padding(14.dp, 6.dp)
+                            .size(24.dp)
+                    )
+                }
+                Text(
+                    text = "Gamer Calls",
+                    style = MaterialTheme.typography.titleSmall,
+                    color = colorResource(id = R.color.SubliminalText),
+                    fontSize = 10.sp,
+                    modifier = modifier
+//                            .padding(start = 12.dp, top = 2.dp, end = 16.dp)
+                )
+            }
+
+
+            Spacer(modifier = modifier.weight(1f))
+
+            Column(modifier = modifier
+                .padding(top = 4.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Row(modifier = modifier
+                    .background(color = colorResource(id = R.color.black),
+                        shape = RoundedCornerShape(6.dp))
+                    .border(
+                        width = 0.5.dp,
+                        color = colorResource(id = R.color.CallWidgetBorder),
+                        shape = RoundedCornerShape(6.dp)
+                    )
+                ) {
+                    Image(
+                        painter = painterResource(id = R.drawable.link_blue),
+                        contentDescription = "Share",
+                        modifier = modifier
+                            .padding(14.dp, 6.dp)
+                            .size(30.dp)
+                    )
+                }
+                Text(
+                    text = "PartyUp!",
+                    style = MaterialTheme.typography.titleSmall,
+                    color = colorResource(id = R.color.SubliminalText),
+                    fontSize = 10.sp,
+                    modifier = modifier
+//                            .padding(start = 12.dp, top = 2.dp, end = 16.dp)
                 )
             }
 
             Spacer(modifier = modifier.weight(1f))
 
-            Row(modifier = modifier
-                .background(color = colorResource(id = R.color.black),
-                    shape = RoundedCornerShape(6.dp))
-                .border(
-                    width = 1.dp,
-                    color = colorResource(id = R.color.CallWidgetBorder),
-                    shape = RoundedCornerShape(6.dp)
-                )
+            Column(modifier = modifier
+                .padding(top = 4.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
             ) {
-                Image(
-                    painter = painterResource(id = R.drawable.hastagthick_blue),
-                    contentDescription = "Share",
+                Row(modifier = modifier
+                    .background(color = colorResource(id = R.color.black),
+                        shape = RoundedCornerShape(6.dp))
+                    .border(
+                        width = 0.5.dp,
+                        color = colorResource(id = R.color.CallWidgetBorder),
+                        shape = RoundedCornerShape(6.dp)
+                    )
+                ) {
+                    Image(
+                        painter = painterResource(id = R.drawable.messagefilled_blue),
+                        contentDescription = "Share",
+                        modifier = modifier
+                            .padding(14.dp, 6.dp)
+                            .size(24.dp)
+                    )
+                }
+
+                Text(
+                    text = "Chats",
+                    style = MaterialTheme.typography.titleSmall,
+                    color = colorResource(id = R.color.SubliminalText),
+                    fontSize = 10.sp,
                     modifier = modifier
-                        .padding(12.dp, 4.dp)
-                        .size(24.dp)
+//                            .padding(start = 12.dp, top = 2.dp, end = 16.dp)
                 )
             }
+
 
             Spacer(modifier = modifier.weight(1f))
 
-            Row(modifier = modifier
-                .background(color = colorResource(id = R.color.black),
-                    shape = RoundedCornerShape(6.dp))
-                .border(
-                    width = 1.dp,
-                    color = colorResource(id = R.color.CallWidgetBorder),
-                    shape = RoundedCornerShape(6.dp)
-                )
+            Column(modifier = modifier
+                .padding(top = 4.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
             ) {
-                Image(
-                    painter = painterResource(id = R.drawable.link_blue),
-                    contentDescription = "Share",
+                Row(modifier = modifier
+                    .background(color = colorResource(id = R.color.black),
+                        shape = RoundedCornerShape(6.dp))
+                    .border(
+                        width = 0.5.dp,
+                        color = colorResource(id = R.color.CallWidgetBorder),
+                        shape = RoundedCornerShape(6.dp)
+                    )
+                ) {
+                    Image(
+                        painter = painterResource(id = R.drawable.user_blue),
+                        contentDescription = "Share",
+                        modifier = modifier
+                            .padding(14.dp, 6.dp)
+                            .size(24.dp)
+                    )
+                }
+
+                Text(
+                    text = "Profile",
+                    style = MaterialTheme.typography.titleSmall,
+                    color = colorResource(id = R.color.SubliminalText),
+                    fontSize = 10.sp,
                     modifier = modifier
-                        .padding(12.dp, 4.dp)
-                        .size(24.dp)
+//                            .padding(start = 12.dp, top = 2.dp, end = 16.dp)
                 )
             }
 
-            Spacer(modifier = modifier.weight(1f))
-
-            Row(modifier = modifier
-                .background(color = colorResource(id = R.color.black),
-                    shape = RoundedCornerShape(6.dp))
-                .border(
-                    width = 1.dp,
-                    color = colorResource(id = R.color.CallWidgetBorder),
-                    shape = RoundedCornerShape(6.dp)
-                )
-            ) {
-                Image(
-                    painter = painterResource(id = R.drawable.messagefilled_blue),
-                    contentDescription = "Share",
-                    modifier = modifier
-                        .padding(12.dp, 4.dp)
-                        .size(24.dp)
-                )
-            }
-
-            Spacer(modifier = modifier.weight(1f))
-
-            Row(modifier = modifier
-                .background(color = colorResource(id = R.color.black),
-                    shape = RoundedCornerShape(6.dp))
-                .border(
-                    width = 1.dp,
-                    color = colorResource(id = R.color.CallWidgetBorder),
-                    shape = RoundedCornerShape(6.dp)
-                )
-            ) {
-                Image(
-                    painter = painterResource(id = R.drawable.user_blue),
-                    contentDescription = "Share",
-                    modifier = modifier
-                        .padding(12.dp, 4.dp)
-                        .size(24.dp)
-                )
-            }
 
             Spacer(modifier = modifier.weight(1f))
 
@@ -365,49 +435,6 @@ fun HomepageContent(modifier: Modifier = Modifier) {
         }
     }
 }
-
-
-//@Composable
-//fun CommunityCard_Mini(modifier: Modifier = Modifier) {
-//    Box(
-//        modifier = Modifier
-//            .background(
-//                brush = Brush.verticalGradient(
-//                    colors = listOf(Color.Transparent, Color.Transparent),
-//                    startY = 0.0f
-//                ),
-//                shape = RoundedCornerShape(15.dp)
-//            )
-//            .height(120.dp)
-//            .width(160.dp)
-//            .border(
-//                width = 1.dp,
-//                color = colorResource(id = R.color.CallWidgetBorder),
-//                shape = RoundedCornerShape(10.dp)
-//            )
-//    ) {
-//        Image(
-//            painter = painterResource(id = R.drawable.valorant),
-//            contentDescription = null,
-//            modifier = Modifier
-//                .fillMaxWidth()
-//                .align(Alignment.Center)
-//        )
-//        Box(
-//            modifier = Modifier
-//                .fillMaxSize()
-//                .background(colorResource(id = R.color.black).copy(alpha = 0.5f))
-//        ) {}
-//        Text(
-//            text = "Valorant",
-//            style = MaterialTheme.typography.titleSmall,
-//            color = colorResource(id = R.color.primary),
-//            modifier = Modifier
-//                .background(colorResource(id = R.color.black).copy(alpha = 0.75f))
-//                .align(Alignment.Center)
-//        )
-//    }
-//}
 
 
 @Composable
